d590d82967a32f0e8157e87a823f5d27030de4b4
naxin <name@bjmantis.com>
2021-01-19 11:45:33 +0800

    nacos服务发现、服务配置基础例子

---
+++ nacos-spring-cloud-config-example/src/main/java/com/cycas/NacosSpringCloudConfigExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cycas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringCloudConfigExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringCloudConfigExampleApplication.class, args);
+    }
+
+}
---
+++ nacos-spring-cloud-provider-example/src/main/java/com/cycas/nacosspringclouddiscoveryexample/NacosSpringCloudDiscoveryExampleApplication.java
@@ -0,0 +1,15 @@
+package com.cycas.nacosspringclouddiscoveryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosSpringCloudDiscoveryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringCloudDiscoveryExampleApplication.class, args);
+    }
+
+}
