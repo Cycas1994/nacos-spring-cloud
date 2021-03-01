547031b473ddbd58568e63c83fd2fd607773f8e1
naxin <naxin@bjmantis.com>
2021-03-01 16:17:28 +0800

    kafka producer

--- kafka8999/src/main/java/com/cycas/kafka/controller/ProductController.java
+++ kafka8999/src/main/java/com/cycas/kafka/controller/ProductController.java
@@ -1,4 +1,28 @@
 package com.cycas.kafka.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
 public class ProductController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @GetMapping("/send")
+    public String send(@RequestParam("topic") String topic, @RequestParam("msg") String msg) {
+
+        kafkaTemplate.send(topic, msg);
+        logger.info("发送消息的topic：{}，发送的内容：{}", topic, msg);
+        return "success";
+    }
 }
--- kafka8999/src/main/java/com/cycas/kafka/service/ConsumerService.java
+++ kafka8999/src/main/java/com/cycas/kafka/service/ConsumerService.java
@@ -1,4 +1,19 @@
 package com.cycas.kafka.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConsumerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
+
+    @KafkaListener(topics = "my_first_topic")
+    public void listen(ConsumerRecord<?, ?> record) {
+
+        logger.info("topic:{}, offset:{}, value:{}", record.topic(), record.offset(), record.value());
+    }
 }
